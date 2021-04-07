f42ae6777e9689518bfed5e4d36f851fc7e4b477
hexat <hairon@ifce.edu.br>
2021-04-07 19:32:18 -0300

    Apagar o Index.html e colocar o index.js

---
+++ Index.js
@@ -0,0 +1,108 @@
+<HTML>
+<HEAD>
+<TITLE>Beatles - Hello Goodbye</TITLE>
+</HEAD>
+<BODY>
+<BODY BGCOLOR=Black 
+TEXT=White 
+LINK=Red 
+VLINK=Green
+ALINK=Red>
+
+<img src="HelloGoodbyeUS.jpg" alt="Capa Hello Goodbye" width="250" height="300"> <BR><BR>
+
+<A NAME="TOP">Hello Goodbye</A> <!—Ãncora—>
+
+<PRE>
+You Say Yes 
+I Say No
+You Say Stop 
+And I Say Go Go Go
+Oh No
+
+You Say Goodbye And I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+
+I Say High 
+You Say Low
+You Say Why 
+And I Say I Don't Know
+Oh No
+
+You Say Goodbye And I Say Hello
+(Hello Goodbye Hello Goodbye)
+Hello Hello
+(Hello Goodbye)
+I Don't Know Why You Say Goodbye I Say Hello
+(Hello Goodbye Hello Goodbye)
+Hello Hello
+(Hello Goodbye)
+I Don't Know Why You Say Goodbye I Say Hello
+(Hello Goodbye)
+
+Why Why Why Why Why Why
+Do You Say Goodbye 
+Goodbye 
+Bye Bye Bye Bye
+Oh No
+
+You Say Goodbye And I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+
+You Say Yes(I Say Yes) 
+I Say No
+(To Make Mean No)
+You Say Stop(I Can Stay) 
+And I Say Go Go Go
+(Till It's Time To Go)
+Oh Oh
+Oh No
+
+You Say Goodbye And I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+Hello Hello
+I Don't Know Why You Say Goodbye I Say Hello
+Oh Oh
+Oh Oh
+Hello Oh Oh
+
+Hey La 
+Hey Aloha
+
+Hey La 
+Hey Aloha(Cha Cha Cha)
+
+Hey La 
+Hey Aloha(Ooooohhh)
+
+Hey La 
+Hey Aloha(Hey La)
+
+Hey La 
+Hey Aloha(Cha Cha Cha)
+
+Hey La 
+Hey Aloha(Oooooh Ooh)
+
+Hey La 
+Hey Aloha(Ooh Ah)
+
+Hey La 
+Hey Aloha(Cha Cha)
+
+Hey La 
+Hey Aloha
+
+</PRE>
+<A HREF="#TOP">Inicio</A>
+</BODY>
+</HTML>
